b9e6303afee15e8f14ee38d82c61c5203fc64654
ImagicTheCat <imagicthecat@gmail.com>
2022-01-06 19:19:55 +0100

    Implement reference counting.

--- src/wrapper.c
+++ src/wrapper.c
@@ -6,11 +6,55 @@
 #include <lauxlib.h>
 #include <uv.h>
 
+// Object reference counting.
+
+typedef struct ljuv_object{
+  uint32_t count;
+  void (*destructor)(struct ljuv_object *obj);
+  uv_mutex_t mutex;
+} ljuv_object;
+
+// return < 0 on failure
+int object_init(ljuv_object *obj, void (*destructor)(ljuv_object *obj))
+{
+  obj->count = 1;
+  obj->destructor = destructor;
+  return uv_mutex_init(&obj->mutex);
+}
+void object_retain(ljuv_object *obj)
+{
+  uv_mutex_lock(&obj->mutex);
+  obj->count++;
+  uv_mutex_unlock(&obj->mutex);
+}
+void object_release(ljuv_object *obj)
+{
+  uv_mutex_lock(&obj->mutex);
+  uint32_t count = --obj->count;
+  uv_mutex_unlock(&obj->mutex);
+  if(count == 0){
+    if(obj->destructor) obj->destructor(obj);
+    free(obj);
+  }
+}
+
+// Channel
+
+
+
+// Expose wrapper functions.
+
 typedef struct ljuv_wrapper{
-  int non_empty;
+  int (*object_init)(ljuv_object *obj, void (*destructor)(ljuv_object *obj));
+  void (*object_retain)(ljuv_object *obj);
+  void (*object_release)(ljuv_object *obj);
 } ljuv_wrapper;
 
-static ljuv_wrapper wrapper;
+static ljuv_wrapper wrapper = {
+  object_init,
+  object_retain,
+  object_release
+};
 
 int luaopen_ljuv_wrapper_c(lua_State *L)
 {
